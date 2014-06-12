6cf3d6401d55457001f20b3faf742615d5be58b9
Søren Trudsø Mahon <strudsomahon@ebay.com>
2014-06-12 10:19:38 +0200

    Enabled guest on none localhost for rabbitmq 3.3

--- provision.sh
+++ provision.sh
@@ -10,6 +10,9 @@ apt-get update
 apt-get install -q -y htop vim curl wget
 apt-get install -q -y rabbitmq-server
 
+#permission for guest
+sudo cp /vagrant/rabbitmq.config /etc/rabbitmq/
+
 # RabbitMQ Plugins
 service rabbitmq-server stop
 rabbitmq-plugins enable rabbitmq_management
